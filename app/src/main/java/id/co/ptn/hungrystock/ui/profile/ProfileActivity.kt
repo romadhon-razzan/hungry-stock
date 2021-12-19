10911251f8ea700a80785ec22222d8cdaa2706a1
romadhon-razzan <romadhon28akbar@gmail.com>
2021-12-20 03:03:31 +0700

    implement ui profile

---
+++ app/src/main/java/id/co/ptn/hungrystock/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package id.co.ptn.hungrystock.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.ptn.hungrystock.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
