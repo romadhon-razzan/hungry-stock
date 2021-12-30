b40323ba09e8234a4e6736d99cfadccb667f00c9
Romadhon Akbar <romadhon28akbar@gmail.com>
2021-12-30 21:33:33 +0700

    implementasi ui learning page

--- app/src/main/java/id/co/ptn/hungrystock/ui/main/learning/LearningFragment.kt
+++ app/src/main/java/id/co/ptn/hungrystock/ui/main/learning/LearningFragment.kt
@@ -6,27 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import id.co.ptn.hungrystock.R
+import id.co.ptn.hungrystock.databinding.LearningFragmentBinding
 
+@AndroidEntryPoint
 class LearningFragment : Fragment() {
 
     companion object {
         fun newInstance() = LearningFragment()
     }
 
-    private lateinit var viewModel: LearningViewModel
+    private lateinit var binding: LearningFragmentBinding
+    private val viewModel: LearningViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.learning_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(LayoutInflater.from(requireContext()), R.layout.learning_fragment, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LearningViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
