1e2b5db190d4342dac6a9d43a77a5cf2643ed045
Romadhon Akbar <romadhon28akbar@gmail.com>
2022-04-14 21:10:42 +0700

    fix penyesuaian

--- app/src/main/java/id/co/ptn/hungrystock/App.kt
+++ app/src/main/java/id/co/ptn/hungrystock/App.kt
@@ -1,9 +1,13 @@
 package id.co.ptn.hungrystock
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.view.WindowManager
 import com.onesignal.OneSignal
 import dagger.hilt.android.HiltAndroidApp
 import id.co.ptn.hungrystock.config.ONESIGNAL_APP_ID
+
 
 @HiltAndroidApp
 class App: Application() {
@@ -16,5 +20,26 @@ class App: Application() {
         // OneSignal Initialization
         OneSignal.initWithContext(this)
         OneSignal.setAppId(ONESIGNAL_APP_ID)
+
+        setupActivityListener()
     }
+
+    private fun setupActivityListener() {
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                activity.window.setFlags(
+                    WindowManager.LayoutParams.FLAG_SECURE,
+                    WindowManager.LayoutParams.FLAG_SECURE
+                )
+            }
+
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+    }
+
 }
