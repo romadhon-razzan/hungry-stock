b0ddfedc76bf190644a5ea1c2fd1cec7ab3000f2
Romadhon Akbar <romadhon28akbar@gmail.com>
2022-04-23 15:06:38 +0700

    persiapan rilis versi 1.1.2

--- app/src/main/java/id/co/ptn/hungrystock/config/ApplicationConfig.kt
+++ app/src/main/java/id/co/ptn/hungrystock/config/ApplicationConfig.kt
@@ -7,5 +7,5 @@ const val ASSET_URL = "https://hungrystock.sgp1.digitaloceanspaces.com/"
 const val PRIVACY_POLICY_URL = "https://hungrystock.com/privacy-policy"
 var TOKEN = ""
 
-const val DEBUG = true
+const val DEBUG = false
 const val ONESIGNAL_APP_ID = "615077a5-db98-4957-85e7-283da51c6f37"
