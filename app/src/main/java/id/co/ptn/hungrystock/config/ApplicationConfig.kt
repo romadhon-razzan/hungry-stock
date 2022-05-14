22051ae8b66375fe8a232517181ec24cc32f4a43
Romadhon Akbar <romadhon28akbar@gmail.com>
2022-05-14 14:30:37 +0700

    rilis versi 1.1.4

--- app/src/main/java/id/co/ptn/hungrystock/config/ApplicationConfig.kt
+++ app/src/main/java/id/co/ptn/hungrystock/config/ApplicationConfig.kt
@@ -8,5 +8,5 @@ const val PRIVACY_POLICY_URL = "https://hungrystock.com/privacy-policy"
 const val MEMBERSHIP = "https://hungrystock.com/membership"
 var TOKEN = ""
 
-const val DEBUG = true
+const val DEBUG = false
 const val ONESIGNAL_APP_ID = "615077a5-db98-4957-85e7-283da51c6f37"
